d4279568fb6b74877338ef4b1fafa75b6f90d505
shangzhikeji <shangzhikeji@gmail.com>
2019-01-24 17:41:16 +0800

    Fix the bug.For more details https://github.com/stephenmcd/mezzanine/issues/1899

--- mezzanine/generic/fields.py
+++ mezzanine/generic/fields.py
@@ -203,7 +203,7 @@ class KeywordsField(BaseGenericRelation):
             data = [related_manager.create(keyword_id=i) for i in new_ids]
         # Remove keywords that are no longer assigned to anything.
         Keyword.objects.delete_unused(removed_ids)
-        super(KeywordsField, self).save_form_data(instance, data)
+        #super(KeywordsField, self).save_form_data(instance, data)
 
     def contribute_to_class(self, cls, name):
         """
